e85611e1331a9958a096553433532b96b60f8326
Luiz Passos <lsps1019@gmail.com>
2023-02-06 20:09:42 -0300

    Update script.js

--- xpe/mega-sena/js/script.js
+++ xpe/mega-sena/js/script.js
@@ -16,5 +16,50 @@ function addNumberToGame(numberToAdd) {
     console.error("O jogo já está completo.");
     return;
   }
+
+  if (isNumberInGame(numberToAdd)) {
+    console.error("Este número já foi selecionado.", numberToAdd);
+    return;
+  }
+
   state.currentGame.push(numberToAdd);
 }
+
+function removeNumberFromGame(numberToRemove) {
+  if (numberToRemove < 1 || numberToRemove > 60) {
+    console.error("Número inválido", numberToRemove);
+    return;
+  }
+
+  var newGame = [];
+
+  for (var i = 0; i < state.currentGame.length; i++) {
+    var currentNumber = state.currentGame[i];
+
+    if (currentNumber === numberToRemove) {
+      continue;
+    }
+
+    newGame.push(currentNumber);
+  }
+
+  state.currentGame = newGame;
+}
+
+function isNumberInGame(numberToCheck) {
+  return state.currentGame.includes(numberToCheck);
+}
+
+function saveGame() {
+  if (!isGameComplete()) {
+    console.error("O jogo não está completo.");
+    return;
+  }
+
+  state.savedGames.push(state.currentGame);
+}
+
+function isGameComplete() {
+  return state.currentGame.length === 6;
+}
+
