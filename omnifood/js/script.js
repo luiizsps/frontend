c9bae8c004edf8d88df44c4cf1c3e21a7313bc54
luiizsps <lsps1019@gmail.com>
2023-02-18 22:24:44 -0300

    adding mobile nav button funcionality

--- omnifood/js/script.js
+++ omnifood/js/script.js
@@ -1,11 +1,10 @@
 // set current year
 const yearEl = document.querySelector(".year");
-console.log(yearEl);
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = documen.querySelector(".header");
+const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
