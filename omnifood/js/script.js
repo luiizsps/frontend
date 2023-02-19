2b6022d6831ef8f8f5fc070ce8243ffa155251d6
luiizsps <lsps1019@gmail.com>
2023-02-18 22:21:14 -0300

    added mobile nav button

--- omnifood/js/script.js
+++ omnifood/js/script.js
@@ -4,4 +4,8 @@ console.log(yearEl);
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
-const btnNav = document.querySelector(".btn-mobile-nav");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = documen.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
