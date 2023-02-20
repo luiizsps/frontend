9475706fc11e7845805b8083e4b03a1081732767
luiizsps <lsps1019@gmail.com>
2023-02-19 22:05:29 -0300

    adding smoth scroll

--- omnifood/js/script.js
+++ omnifood/js/script.js
@@ -8,3 +8,32 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //scroll back to the top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    //scroll to other places
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector("href");
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
