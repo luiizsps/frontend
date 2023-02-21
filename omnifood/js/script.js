fe67566b417971ee0586b6b2f121c3999afeb0df
luiizsps <lsps1019@gmail.com>
2023-02-20 22:17:39 -0300

    implementing sticky navigation bar

--- omnifood/js/script.js
+++ omnifood/js/script.js
@@ -42,10 +42,19 @@ const sectionHeroEl = document.querySelector(".section-hero");
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
   },
   {
     root: null,
     threshold: 0,
+    rootMargin: "-80px",
   }
 );
 obs.observe(sectionHeroEl);
