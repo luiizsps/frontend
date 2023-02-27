7fe88738292abd5c5e11fc487a20e3d45a20263c
luiizsps <lsps1019@gmail.com>
2023-02-26 21:34:09 -0300

    first coding challenge

--- JsCourse/myOwnWork/Section1/js/script.js
+++ JsCourse/myOwnWork/Section1/js/script.js
@@ -1,9 +1,23 @@
 /*let js = "amazing";
 if (js === "amazing") alert("JavaScript is fun");
-*/
+
 const year = 2037;
 const age = year - 2002;
 
 const firstName = "Luiz Sérgio";
 const lastName = "Passos da Silva";
 console.log(`Name: ${firstName} ${lastName}`);
+*/
+
+const firstHeight = document.querySelector("#firstHeight").value;
+const firstWeight = document.querySelector("#firstMass").value;
+const secondHeight = document.querySelector("#secondHeight").value;
+const secondWeight = document.querySelector("#secondMass").value;
+const resultEl = document.querySelector(".result");
+
+firstBMI = Number(firstWeight.value) / Number(firstHeight.value ** 2);
+secondBMI = Number(secondWeight.value) / Number(secondHeight.value ** 2);
+
+if (firstBMI > secondBMI)
+  resultEl.innerHTML = "The first person has a bigger BMI";
+else resultEl.innerHTML = "The second person has a bigger BMI";
