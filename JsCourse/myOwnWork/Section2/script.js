cefaa777719d6e2cc99cdba41a920b1bb7efb18e
luiizsps <lsps1019@gmail.com>
2023-03-04 21:25:31 -0300

    using functions

--- JsCourse/myOwnWork/Section2/script.js
+++ JsCourse/myOwnWork/Section2/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function logger() {
+  console.log("My name is Luiz");
+}
+
+logger();
+
+function fruitProcesser(apples, strawberrys) {
+  console.log(apples, strawberrys);
+
+  juice = `Juice with ${apples} apples and ${strawberrys} strawberrys`;
+  return juice;
+}
+
+juice = fruitProcesser(3, 2);
+console.log(juice);
