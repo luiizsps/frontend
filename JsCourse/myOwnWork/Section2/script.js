ad4cdf0c2c6acca1e179bcead427f17a521a282a
luiizsps <lsps1019@gmail.com>
2023-03-04 21:26:07 -0300

    using functions

--- JsCourse/myOwnWork/Section2/script.js
+++ JsCourse/myOwnWork/Section2/script.js
@@ -9,9 +9,9 @@ logger();
 function fruitProcesser(apples, strawberrys) {
   console.log(apples, strawberrys);
 
-  juice = `Juice with ${apples} apples and ${strawberrys} strawberrys`;
+  const juice = `Juice with ${apples} apples and ${strawberrys} strawberrys`;
   return juice;
 }
 
-juice = fruitProcesser(3, 2);
+const juice = fruitProcesser(3, 2);
 console.log(juice);
