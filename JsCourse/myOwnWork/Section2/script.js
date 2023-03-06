8be525c82fc1514d2392e8313a5a36006274029f
luiizsps <lsps1019@gmail.com>
2023-03-05 22:59:16 -0300

    using arrow functions

--- JsCourse/myOwnWork/Section2/script.js
+++ JsCourse/myOwnWork/Section2/script.js
@@ -26,10 +26,10 @@ const juice = fruitProcesser(3, 2);
 console.log(juice);
 
 const yearsUntilRetiremant = (birthYear, firstName) => {
-  const age = 2037 - birthYear;
+  const age = 2023 - birthYear;
   const retirement = 65 - age;
 
-  return `${firstName} is ${retirement} away from retirement`;
+  return `${firstName} is ${retirement} years away from retirement`;
 };
 
-console.log(yearsUntilRetiremant(2002));
+console.log(yearsUntilRetiremant(2002, "Luiz"));
