80b4a0c963f2b5f5b6a70bd86b3777f703f0586e
luiizsps <lsps1019@gmail.com>
2023-03-08 23:06:14 -0300

    upgrade script.js

--- JsCourse/myOwnWork/Section2/script.js
+++ JsCourse/myOwnWork/Section2/script.js
@@ -11,9 +11,6 @@ const func = function (apples, strawberries) {
   console.log(apples, strawberries);
 };
 
-// arrow function
-const arrow = (birthYear) => 2023 - birthYear;
-
 const juice = fruitProcesser(3, 2);
 console.log(juice);
 
