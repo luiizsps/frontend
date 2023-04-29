7565cc030e606eeb5b57d65f8ec81564db6f5ba9
luiizsps <lsps1019@gmail.com>
2023-04-28 21:38:36 -0300

    array methods

--- JsCourse/myOwnWork/Section2/challenge2.js
+++ JsCourse/myOwnWork/Section2/challenge2.js
@@ -2,19 +2,13 @@ bill = [100, 200, 300, 400, 500];
 const tip = [],
   total = [];
 
-function calcTip(bill, tip) {
+function calcTip(bill, tip, total) {
   for (let i = 0; i < bill.length; i++) {
     if (bill[i] > 50 && bill[i] < 300) tip.push(bill[i] * 0.15);
     else tip.push(bill[i] * 0.2);
-  }
-}
-
-function calcTotal(bill, tip, total) {
-  for (let i = 0; i < bill.length; i++) {
     total.push(bill[i] + tip[i]);
   }
 }
 
-calcTip(bill, tip);
-calcTotal(bill, tip, total);
+calcTip(bill, tip, total);
 console.log(tip, total);
